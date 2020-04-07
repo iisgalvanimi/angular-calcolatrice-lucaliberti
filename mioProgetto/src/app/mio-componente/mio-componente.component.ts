0dc979093baa53a128ae9e50b830f2966239e18c
lucaliberti <liberti.luca@gmail.com>
2020-04-07 16:35:12 +0000

    Agiunto mio componente

---
+++ mioProgetto/src/app/mio-componente/mio-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mio-componente',
+  templateUrl: './mio-componente.component.html',
+  styleUrls: ['./mio-componente.component.css']
+})
+export class MioComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
