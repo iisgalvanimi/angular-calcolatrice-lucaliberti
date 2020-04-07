f383bfd3e50f5e342a71a83834d8a1f3f62c7d51
lucaliberti <liberti.luca@gmail.com>
2020-04-07 16:35:59 +0000

    app.module.ts

--- mioProgetto/src/app/app.module.ts
+++ mioProgetto/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MioComponenteComponent } from './mio-componente/mio-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MioComponenteComponent
   ],
   imports: [
     BrowserModule
